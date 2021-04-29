6989530b2d1d238ade0ceba8c60694ea3162347f
edup94 <eduardo.paredes@estudiantes.utec.edu.uy>
2021-04-29 14:56:43 +0000

    guardado1

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-export function Home() {
+export function Home(props) {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container rounded d-flex justify-content-center text-center text-white mt-5">
+			<div className="numero col-1 m-1 rounded">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="numero col-1 m-1 rounded">{props.sexto}</div>
+			<div className="numero col-1 m-1 rounded">{props.quinto}</div>
+			<div className="numero col-1 m-1 rounded">{props.cuarto}</div>
+			<div className="numero col-1 m-1 rounded">{props.tercero}</div>
+			<div className="numero col-1 m-1 rounded">{props.segundo}</div>
+			<div className="numero col-1 m-1 rounded">{props.primero}</div>
 		</div>
 	);
 }
+
+Home.propTypes = {
+	sexto: PropTypes.number,
+	quinto: PropTypes.number,
+	cuarto: PropTypes.number,
+	tercero: PropTypes.number,
+	segundo: PropTypes.number,
+	primero: PropTypes.number
+};
